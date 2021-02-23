1ec60dc1fe84f4a1e4a99807b0abf4b1a0858cbd
Agustina <69316359+agusaragonn@users.noreply.github.com>
2021-02-23 13:21:24 -0300

    Add files via upload

---
+++ plot4.R
@@ -0,0 +1,27 @@
+# GRAPHIC 4
+
+data4 <- read.table("household_power_consumption.txt",skip=1,sep=";")
+names(data4) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+data4 <- subset(data4,data4$Date=="1/2/2007" | data4$Date =="2/2/2007")
+
+
+data4$Date <- as.Date(data4$Date, format="%d/%m/%Y")
+data4$Time <- strptime(data4$Time, format="%H:%M:%S")
+data4[1:1440,"Time"] <- format(data4[1:1440,"Time"],"2007-02-01 %H:%M:%S")
+data4[1441:2880,"Time"] <- format(data4[1441:2880,"Time"],"2007-02-02 %H:%M:%S")
+
+
+par(mfrow=c(2,2))
+
+with(data4,{
+  plot(data4$Time,as.numeric(as.character(data4$Global_active_power)),type="l",  xlab="",ylab="Global Active Power")  
+  plot(data4$Time,as.numeric(as.character(data4$Voltage)), type="l",xlab="datetime",ylab="Voltage")
+  plot(data4$Time,data4$Sub_metering_1,type="n",xlab="",ylab="Energy sub metering")
+  with(data4,lines(Time,as.numeric(as.character(Sub_metering_1))))
+  with(data4,lines(Time,as.numeric(as.character(Sub_metering_2)),col="red"))
+  with(data4,lines(Time,as.numeric(as.character(Sub_metering_3)),col="blue"))
+  legend("topright", lty=1, col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), cex = 0.6)
+  plot(data4$Time,as.numeric(as.character(data4$Global_reactive_power)),type="l",xlab="datetime",ylab="Global_reactive_power")
+})
+
+png("plot4.png", width=480, height=480)
